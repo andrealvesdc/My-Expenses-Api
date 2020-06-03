4108b225c393fe1d5a7d5784a18c7c97c32b7d22
André Alves <andrealvesdc@gmail.com>
2020-06-03 17:13:03 -0300

    commit inicial e conexão com database

---
+++ src/main/java/com/andrealves/myexpenses/api/MyExpensesApiApplication.java
@@ -0,0 +1,13 @@
+package com.andrealves.myexpenses.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExpensesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExpensesApiApplication.class, args);
+	}
+
+}
