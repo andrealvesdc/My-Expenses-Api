70900f6bc9cc791fb781bee1ef0205e048c6d772
Andre Alves <andrealvesdc@gmail.com>
2020-06-14 19:25:40 -0300

    GetById pessoa

--- src/main/java/com/andrealves/myexpenses/api/resource/PessoaResource.java
+++ src/main/java/com/andrealves/myexpenses/api/resource/PessoaResource.java
@@ -1,18 +1,22 @@
 package com.andrealves.myexpenses.api.resource;
 
 import java.net.URI;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.andrealves.myexpenses.api.model.Categoria;
 import com.andrealves.myexpenses.api.model.Pessoa;
 import com.andrealves.myexpenses.api.repository.PessoaRepository;
 
@@ -32,5 +36,16 @@ public class PessoaResource {
 			response.setHeader("Location", uri.toASCIIString());
 			
 			return ResponseEntity.created(uri).body(pessoaSalva);
+	}
+	
+	@GetMapping(value = "/{codigo}")
+	public ResponseEntity<Optional<Pessoa>> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+		
+		if(pessoa.isPresent()) {
+			return ResponseEntity.ok(pessoa);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
