0735bfcfe6c03d1340031838d3b809e3c4b1be4c
Andre Alves <andrealvesdc@gmail.com>
2020-06-24 16:05:46 -0300

    Atualização de usuário com put

--- src/main/java/com/andrealves/myexpenses/api/resource/PessoaResource.java
+++ src/main/java/com/andrealves/myexpenses/api/resource/PessoaResource.java
@@ -1,5 +1,6 @@
 package com.andrealves.myexpenses.api.resource;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,43 +24,56 @@ import org.springframework.web.bind.annotation.RestController;
 import com.andrealves.myexpenses.api.event.RecursoCriadoEvent;
 import com.andrealves.myexpenses.api.model.Pessoa;
 import com.andrealves.myexpenses.api.repository.PessoaRepository;
+import com.andrealves.myexpenses.api.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
-public class PessoaResource {
-	
+public class PessoaResource implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
+	@Autowired
 	private ApplicationEventPublisher publisher;
-	
+
+	@Autowired
+	private PessoaService pessoaService;
+
 	@GetMapping
-	public List<Pessoa> listar(){
+	public List<Pessoa> listar() {
 		return pessoaRepository.findAll();
-	} 
-	
+	}
+
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
-	
+
 	@GetMapping(value = "/{codigo}")
 	public ResponseEntity<Optional<Pessoa>> buscarPeloCodigo(@PathVariable Long codigo) {
 		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
-		
-		if(pessoa.isPresent()) {
+
+		if (pessoa.isPresent()) {
 			return ResponseEntity.ok(pessoa);
-		}else {
+		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	
+
 	@DeleteMapping("/{codigo}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long codigo) {
 		pessoaRepository.deleteById(codigo);
 	}
+
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+		return ResponseEntity.ok(pessoaSalva);
+	}
+
 }
---
+++ src/main/java/com/andrealves/myexpenses/api/service/PessoaService.java
@@ -0,0 +1,26 @@
+package com.andrealves.myexpenses.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.andrealves.myexpenses.api.model.Pessoa;
+import com.andrealves.myexpenses.api.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaRepository.getOne(codigo);
+		if (pessoaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}
+
+}
